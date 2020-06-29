544b563e9023df51fb67c3f5d53df843c4ba4b63
Kajal Gaikwad <66130116+kdgaikwad@users.noreply.github.com>
2020-06-29 17:19:29 +0530

    Update ECommerceWebsite.java

--- src/main/java/baseClass/ECommerceWebsite.java
+++ src/main/java/baseClass/ECommerceWebsite.java
@@ -15,7 +15,6 @@ import com.aventstack.extentreports.markuputils.ExtentColor;
 import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
-
 import utilities.Constant;
 import utilities.ExcelUtils;
 import utilities.Screenshot;
@@ -25,49 +24,49 @@ public class ECommerceWebsite {
 	public static ExtentTest test;
 	public static ExtentReports extent;
 	public static ExtentHtmlReporter htmlReport;
-	
+
 	String Reportfile = System.getProperty("user.dir")+"/test-output/HTMLReport.html";
-	
+
 	public void reportGenerate() throws Exception{
 		htmlReport = new ExtentHtmlReporter(Reportfile);
 		extent = new ExtentReports();
 		extent.attachReporter(htmlReport);
 		htmlReport.config().setReportName("Biba Shopping Website Automation Testing ");
 		htmlReport.config().setTheme(Theme.STANDARD);
-	    htmlReport.config().setDocumentTitle("HtmlReportsTestResults");
+		htmlReport.config().setDocumentTitle("HtmlReportsTestResults");
 	}
-	
+
 	public void captureScreenshot(ITestResult result) throws IOException {
 		test = extent.createTest(result.getName());
 		if(result.getStatus()==ITestResult.SUCCESS) {
 			test.log(Status.PASS, MarkupHelper.createLabel("Test Case Passed", ExtentColor.GREEN));
 		}
 		else if(result.getStatus()==ITestResult.FAILURE) {
-				String temp = Screenshot.screenshot(driver, result.getName());
-				test.log(Status.FAIL, MarkupHelper.createLabel("Test Case Failed", ExtentColor.RED));
-				test.addScreenCaptureFromPath(temp);
-		
+			String temp = Screenshot.screenshot(driver, result.getName());
+			test.log(Status.FAIL, MarkupHelper.createLabel("Test Case Failed", ExtentColor.RED));
+			test.addScreenCaptureFromPath(temp);
+
 		}
 		else {
 			test.log(Status.SKIP, MarkupHelper.createLabel("Test Method Skiped", ExtentColor.GREY));
 		}
 		extent.flush();
 	}
-	
+
 	public void openBrowser() throws Exception {
 
 		Reporter.log("Testcases Exceution Report For Website", true);
-        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        driver.get("https://www.biba.in/"); 
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.get("https://www.biba.in/"); 
 		driver.manage().window().maximize();
-		
+
 	}
-	
-	
-	
-	public void signInPage() throws Exception {
+
+
+	public void signInPage() throws Exception
+	{
 		//identify signin button
 		WebElement signin = driver.findElement(By.xpath("//*[@id=\"myaccount\"]/div/div/a[1]"));
 		signin.click();
@@ -76,7 +75,7 @@ public class ECommerceWebsite {
 		String Username = ExcelUtils.getCellData(1, 1);
 		//identify username element
 		WebElement username = driver.findElement(By.xpath("//*[@id=\"ctl00_ContentPlaceHolder1_ctl00_ctl01_Login1_UserName\"]")); 
-		if(username.isDisplayed())
+		if(username.isDisplayed()) 
 			username.click();
 		username.sendKeys(Username);
 		//fetching password from excel
@@ -91,11 +90,11 @@ public class ECommerceWebsite {
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
 
-}
-  
-  
-	public void productSelection() throws Exception {
-        //enter productname from excelsheet
+	}
+
+	public void productSelection() throws Exception
+	{
+		//enter productname from excelsheet
 		String serch = ExcelUtils.getCellData(1, 3);
 		//identify searchinput box 
 		WebElement searchBox = driver.findElement(By.xpath("//*[@id=\"txtSearch\"]"));
@@ -107,69 +106,69 @@ public class ECommerceWebsite {
 			searchBox.sendKeys(serch);
 			searchButton.click();
 		}
-		 //identify addToBag element
-		  WebElement addToBag =driver.findElement(By.xpath("//*[@id=\"3241494\"]/div[2]/div/div[2]/div/h2/a"));
-		  addToBag.click();
+		//identify addToBag element
+		WebElement addToBag =driver.findElement(By.xpath("//*[@id=\"3241494\"]/div[2]/div/div[2]/div/h2/a"));
+		addToBag.click();
 	}
-	
 
+    
+	public void addToCart() throws Exception
+	{
 
-  public void addToCart() throws Exception{
-	 
-	  //identify size element
-	  WebElement size = driver.findElement(By.xpath("//*[@id=\"236992\"]/span"));
-	  size.click();
-	  //identify addToCart element
-	  WebElement addToCart = driver.findElement(By.xpath("//*[@id=\"pdmainDiv\"]/div/div/div[1]/div/div[2]/div[5]/div[2]/div[2]/div/div[3]/div[1]/span[2]/input"));
-	  addToCart.click();
-	  driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-	  //identify close popup
-	  WebElement closePopup= driver.findElement(By.xpath("//*[@id=\"QuickCart\"]/div[1]/a"));
-	  if(closePopup.isDisplayed())
-	  {
-		  closePopup.click();
-		  Reporter.log("-----addTocart Testcase completed successfully-----");
+		//identify size element
+		WebElement size = driver.findElement(By.xpath("//*[@id=\"236992\"]/span"));
+		size.click();
+		//identify addToCart element
+		WebElement addToCart = driver.findElement(By.xpath("//*[@id=\"pdmainDiv\"]/div/div/div[1]/div/div[2]/div[5]/div[2]/div[2]/div/div[3]/div[1]/span[2]/input"));
+		addToCart.click();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		//identify close popup
+		WebElement closePopup= driver.findElement(By.xpath("//*[@id=\"QuickCart\"]/div[1]/a"));
+		if(closePopup.isDisplayed())
+		{
+			closePopup.click();
+			Reporter.log("-----addTocart Testcase completed successfully-----");
 
-		  driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-	  }
-	  else
-	  {
-		  Reporter.log("testcase addTocart is failed.....");
+			driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		}
+		else
+		{
+			Reporter.log("testcase addTocart is failed.....");
 
-	  }
-  }
+		}
+	}
 
- 
+    public void logout() throws Exception
+	{
 
-  public void logout() throws Exception{
+		WebElement myAccount = driver.findElement(By.xpath("//*[@id=\"lblusrn\"]"));
+		myAccount.click();
+		WebElement logout  = driver.findElement(By.xpath("//*[@id=\"lnkLogout1\"]"));
+		if(logout.isDisplayed())
+		{
+			logout.click();
+			driver.quit();
+			Reporter.log("-----logout Testcase completed successfully-----");
+		}
+		else
+		{
+			Reporter.log("testcase logout is failed.....");
+		}
+	}
 
-	  WebElement myAccount = driver.findElement(By.xpath("//*[@id=\"lblusrn\"]"));
-	  myAccount.click();
-	  WebElement logout  = driver.findElement(By.xpath("//*[@id=\"lnkLogout1\"]"));
-	  if(logout.isDisplayed())
-	  {
-		  logout.click();
-		  driver.quit();
-		  Reporter.log("-----logout Testcase completed successfully-----");
-	  }
-	  else
-	  {
-		  Reporter.log("testcase logout is failed.....");
-	  }
-  }
+	public void closeBrowser() throws InterruptedException
+	{
 
-  public void closeBrowser() throws InterruptedException {
+		//sleep java thread for 4 seconds
+		Thread.sleep(4000); 
+		//close all the driver instances
+		driver.quit();
 
-	  //sleep java thread for 4 seconds
-	  Thread.sleep(4000); 
-	  //close all the driver instances
-	  driver.quit();
-
-  }
+	}
 }
 
 
 
-	
+
 
 
